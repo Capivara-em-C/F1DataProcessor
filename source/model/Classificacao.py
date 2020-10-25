2afbdd54743819d7c0d9e3d59ed3a2526f8c6e06
Guilherme-Sodre <38117734+Guilherme-Sodre@users.noreply.github.com>
2020-10-24 22:52:07 -0300

    Create Classificacao.py

---
+++ source/model/Classificacao.py
@@ -0,0 +1,7 @@
+class Classificacao:
+    def __init__(self, standinId, driverId, year, nationality, equipe):
+        self.standinId = standinId
+        self.driverId = driverId
+        self.year = year
+        self.nationality = nationality
+        self.equipe = equipe
